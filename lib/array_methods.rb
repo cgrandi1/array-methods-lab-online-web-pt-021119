a2b52ad07a4547469faf95a5684fcb1e5e944b7d
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-17 17:51:28 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include? ("Tardis!")
+  array.include? ("Tardi")
   array.include? ("Tardis")
 end
 
